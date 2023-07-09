64620cd0b17bb777068d862ba708c398edb981ae
glushkov <klepa_82@bk.ru>
2023-07-09 18:50:24 +0300

    add task 1

---
+++ task2.java
@@ -0,0 +1,49 @@
+// Задание
+
+// 1) Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, используя StringBuilder или String. Данные для фильтрации приведены ниже в виде json-строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+
+// Дополнительные задания
+
+// 2) Дана json-строка (можно сохранить в файл и читать из файла)
+// [{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+// Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида: Студент [фамилия] получил [оценка] по предмету [предмет].
+// Пример вывода:
+// Студент Иванов получил 5 по предмету Математика.
+// Студент Петрова получил 4 по предмету Информатика.
+// Студент Краснов получил 5 по предмету Физика.
+
+package seminar_2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 
+Формируем WHERE
+*/
+public class task2 {
+    public static void main(String[] args) {
+        Map<String, String> params1 = new HashMap<String,String>();
+        params1.put("name","Ivanov");
+        params1.put("country","Ukraine");
+        params1.put("city","Kiev");
+        params1.put("age",null);
+        System.out.println(getQuery(params1));
+    }
+    public static String getQuery(Map<String, String> params)
+    {
+        StringBuilder s = new StringBuilder();
+        for (Map.Entry<String,String> pair : params.entrySet())
+        {
+            if (pair.getValue() != null)
+            {
+                s.append(pair.getKey() +" = '" + pair.getValue()+"' and ");
+            }
+        }
+        s.delete(s.toString().length()-5,s.toString().length());
+        return s.toString();
+    }
+
+}
+
